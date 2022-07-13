2a75ca5fb01bfce9bbf08dc6ae99c61c69762180
Mazzie <mazzieola@hotmail.com>
2022-07-13 23:31:05 +0200

    Jenkins

---
+++ test_tdd/test.py
@@ -0,0 +1,28 @@
+import unittest
+import calc as calcClass
+
+class TestCalc(unittest.TestCase):
+
+    # def test_add(self):
+    #     result = calc.add(10, 5)
+    #     self.assertEqual(result, 15)
+    #     self.assertEqual(result, 15)
+
+    def test_add(self):
+        self.assertEqual(calcClass.calc.add(10, 5), 15)
+        self.assertEqual(calcClass.calc.add(-1, 1), 0)
+        self.assertEqual(calcClass.calc.add(-1, -1), -2)
+
+    # def test_subtract(self):
+    #     self.assertEqual(calcClass.calc.subtract(10, 5), 5)
+    #     self.assertEqual(calcClass.calc.subtract(-1, 1), -2)
+    #     self.assertEqual(calcClass.calc.subtract(-1, -1), 0)
+
+
+    # def test_multiply(self):
+    #     self.assertEqual(calcClass.calc.multiply(10, 5), 50)
+    #     self.assertEqual(calcClass.calc.multiply(-1, 1), -1)
+    #     self.assertEqual(calcClass.calc.multiply(-1, -1), 1)
+
+if __name__ == '__main__':
+    unittest.main
