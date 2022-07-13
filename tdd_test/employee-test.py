389b4f6eb54cf8a3bc0ae5aac0f76c00ca01e938
Mazzie <mazzieola@hotmail.com>
2022-07-13 21:47:29 +0200

    changes

--- tdd_test/employee-test.py
+++ tdd_test/employee-test.py
@@ -5,5 +5,4 @@ class EmployeeTest(unittest.TestCase):
     def add_Employee(self):
         new_emp = Employee("Jane", "j@j.com", "user1", "x1234x")
         self.assertIsNotNone(new_emp)
-        message = new_emp()
-        print(message)
+       
