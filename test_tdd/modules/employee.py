38b7a4e8b0d086deedf51cbc8496606ac59ef26d
Mazzie <mazzieola@hotmail.com>
2022-07-13 22:37:17 +0200

    employees

---
+++ test_tdd/modules/employee.py
@@ -0,0 +1,10 @@
+class Employee:
+
+    def __init__(self, name, passw, email, username):
+        self.name = name
+        self.email = email
+        self.username = username
+        self.passw = passw
+
+    def addEmployee(self):
+        return f"Please create a {self.username} and {self.passw}"
---
+++ test_tdd/modules/test_employee.py
@@ -0,0 +1,7 @@
+import unittest
+from employee import Employee
+
+class EmployeeTest(unittest.TestCase):
+    def add_Employee(self):
+        new_emp = Employee("Jane", "j@j.com", "user1", "x1234x")
+        self.assertIsNotNone(new_emp)
