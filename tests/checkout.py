9f1b6fd7436209b3cdc0e789f5de50eaf4bbec34
mazzielafa <mazzieola@hotmail.com>
2022-08-12 10:47:38 +0200

    checkout-add-discounts

--- tests/checkout.py
+++ tests/checkout.py
@@ -1,10 +1,53 @@
+from json import tool
+
+
 class Checkout:
+    class Discount:
+        def __init__(self, numOfItems, price):
+            self.numOfItems = numOfItems
+            self.price = price
+
+    def __init__(self):
+        self.prices  = {}
+        self.discounts = {}
+        self.items = {}
+
+    def addDiscount(self, item, numOfItems, price):
+        discount = self.Discount(numOfItems, price)
+        self.discounts[item] = discount
 
     def addItemPrice(self, item, price):
+        self.prices[item] = price
         pass
 
     def addItem(self, item):
-        pass
+        if item in self.items:
+            self.items[item] += 1
+        else:
+            self.items[item] = 1
+
 
     def calcTotal(self):
-        return 1
+        total = 0
+        for item, cnt in self.items.items():
+            total += self.calcItemTotal(item, cnt )
+        return total
+
+    def calcItemTotal(self, item, cnt):
+        total = 0
+        if item in self.discounts:
+            discount = self.discounts[item]
+            if cnt >= discount.numOfItems:
+                total += self.calcItemDiscount(item, cnt, discount)
+            else:
+                total += self.prices[item] * cnt
+        else:
+            total += self.prices[item] * cnt
+        return total
+
+    def calcItemDiscount(self, item, cnt, discount):
+        numOfDiscounts = cnt/discount.numOfItems
+        total += numOfDiscounts * discount.price
+        remaining = cnt % discount.numOfItems
+        total += remaining * self.prices[item]
+        return total
--- tests/test_checkout.py
+++ tests/test_checkout.py
@@ -1,12 +1,28 @@
-from tabnanny import check
 from checkout import Checkout
 
 
 def checkout():
     checkout = Checkout()
+    checkout.addItemPrice("a", 1)
+    checkout.addItemPrice("b", 2)
     return checkout
 
 def test_calcTotal(checkout):
-    checkout.addItemProce("a",1)
+    checkout.addItemPrice("a", 1)
     checkout.addItem("a")
     assert checkout.calcTotal() == 1
+
+def test_totalMultipleItems(checkout):
+    checkout.addItem("a")
+    checkout.addItem("b")
+    assert checkout.calcTotal() == 3
+
+def test_addDiscount(checkout):
+    checkout.addDiscount("a", 3, 2)
+
+def test_applyDiscount(checkout):
+    checkout.addDiscount("a", 3, 2)
+    checkout.addItem("a")
+    checkout.addItem("a")
+    checkout.addItem("a")
+    assert checkout.calcTotal() == 2 
